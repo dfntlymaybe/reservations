c1fdb42bf7a5d4017defff2d343f62d3fbb99552
Maor <maorfeldinger@gmail.com>
2016-11-05 21:36:17 +0200

    Add Reservation log on the HTML

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 var reservations = {
-  'Bob': { claimed: false },
-  'Ted': { claimed: true }
+  'bob': { claimed: false },
+  'ted': { claimed: true }
 }
 
-var name = prompt('Please enter the name for your reservation');
+
 
 var claimReservation = function () {
-  // write your code here!
+  var name = document.getElementById("name").value.toLowerCase();
+  console.log(name);
+  if (reservations[name] && reservations[name]["claimed"]){
+    document.getElementById("respons").innerHTML = "This reservation has already been claimed";
+    //alert("This reservation has already been claimed");
+  }else if(reservations[name] && !reservations[name]["claimed"]){
+    document.getElementById("respons").innerHTML = "Welcome Mr. " + name.toUpperCase();
+    reservations[name] = {claimed: true};
+      //alert("Welcome Mr. " + name);
+  }else{
+    //alert("Hey Mr " + name + ", there is no reservation under your name but its your lucky day we have a table for you");
+    document.getElementById("respons").innerHTML = "Hey Mr " + name.toUpperCase() + ", there is no reservation under your name but its your lucky day we have a table for you";
+    reservations[name] = {claimed: true};
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode("New reservation for: " + name.toUpperCase() + " (" + Date() + ")");
+    node.appendChild(textnode);
+    document.getElementById("reservations-list").appendChild(node);
+    //console.log(reservations);
+  }
 }
 
-claimReservation();
+//claimReservation();
